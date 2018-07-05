919c8596525f3eb47fa9be02893f8603a47c3687
Tomoyuki Sakurai <y@trombik.org>
2018-07-05 19:01:33 +0900

    sync test app with the one in esp-idf

--- main/main.c
+++ main/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "unity.h"
+#include "unity_config.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
@@ -7,13 +8,12 @@ void
 unityTask(void *pvParameters) {
 	vTaskDelay(1000 / portTICK_PERIOD_MS);
 	unity_run_menu();
-	while (1);
 }
 
 void
 app_main() {
 	/* Note: if unpinning this task, change the way run times are calculated
 	 * in unity_platform */
-	xTaskCreatePinnedToCore(unityTask, "unityTask", 8192, NULL,
+	xTaskCreatePinnedToCore(unityTask, "unityTask", UNITY_FREERTOS_STACK_SIZE, NULL,
 	    UNITY_FREERTOS_PRIORITY, NULL, UNITY_FREERTOS_CPU);
 }
